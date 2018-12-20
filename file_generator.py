50976c1c79c7e18f0403f0b315fc714738097641
ilker <ilkeraycicek36@gmail.com>
2018-12-20 16:20:13 +0300

    Make 5 mb file

--- file_generator.py
+++ file_generator.py
@@ -4,9 +4,6 @@
 big_file = open("demofile.txt","r")
 little_file = open("demofile_light.txt","w+")
 counter = 10000
-for i in range(0,1500):
+for i in range(0,95000):
     line = big_file.readline()
     little_file.write(line)
-    counter-=1
-    if(counter == 0):
-        break
